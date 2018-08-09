412cff3a530ee2e86bac425453198de785ce4001
Andrew Woodward <ebenwoodward@icloud.com>
2018-08-09 02:59:06 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
 
 class Artist
 
   attr_accessor :name
 
 end
-=======
-require "pry"
-
-class Artist
-
-  attr_reader :name
-
-  def initialize(name)
-    binding.pry
-    @name = name
-  end
-
-end
->>>>>>> eec24b4345faabe8b1732b129e9c9ed6788be893
